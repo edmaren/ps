20d379831e17127065792f224773373b5cb03080
edmaren <134834112+edmaren@users.noreply.github.com>
2025-03-01 00:13:32 +0100

    Add files via upload

---
+++ payload_map.js
@@ -0,0 +1,28 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'etaHEN',
+            description: 'AIO HEN',
+            fileName: 'etaHEN.bin',
+            author: 'LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '2.0b',
+        },
+        {
+            displayTitle: 'Versions',
+            description: 'Shows kernel build, os and sdk versions',
+            fileName: 'versions.elf',
+            author: 'SiSTRo',
+            source:'https://github.com/SiSTR0/ps5-versions/releases/download/v1.0/ps5-versions.elf',
+            version: '1.0'
+        },
+        {
+            displayTitle: 'PS5_Browser_appCache_remove',
+            description: 'Removes appcache from browser.',
+            fileName: 'Browser_appCache_remove.elf',
+            author: 'Storm21CH',
+            source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
+            version: '1.0fix'
+        }
+
+    ];
